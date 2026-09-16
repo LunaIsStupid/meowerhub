08983ca82cf3d52c8571335be5226ef1daf35b7d
Luna <luna.is.stupider@gmail.com>
2026-09-16 02:20:25 -0400

    Include the _settings.py file (i forgot to include this)

---
+++ cogs/fun/_settings.py
@@ -0,0 +1,15 @@
+import discord
+
+# Shared settings file
+
+EVERYONE_PETPET: list[str] = [
+    "@everyone",
+    "everyone",
+    "all",
+    "@here",
+    "@*",
+    "*",
+    "every1",
+    "42,2A,052",
+]
+DEFAULT_COLOR = discord.Colour(0xFFAAFF)
