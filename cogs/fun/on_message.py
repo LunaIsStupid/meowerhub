7664964827d64c14cdedebf52a8769223e777f54
Luna <luna.is.stupider@gmail.com>
2026-09-18 11:11:18 -0400

    Check for 'hi' in the message rather than just the start

--- cogs/fun/on_message.py
+++ cogs/fun/on_message.py
@@ -30,7 +30,7 @@ class OnMessage(commands.Cog):
         if message.author.bot: return
 
         # on mentioned
-        if self.bot.user in message.mentions and message.content.startswith("hi"):
+        if self.bot.user in message.mentions and "hi" in message.content.lower():
             return await message.reply(self.MENTION_REPLY)
 
         # dice
